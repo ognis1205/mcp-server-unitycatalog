8fd135032ed40a35ee9ea3ca286273199d5e52c9
Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>
2025-02-12 05:19:56 +0900

    Start implementing tools module.
    Signed-off-by: Shingo OKAWA <shingo.okawa.g.h.c@gmail.com>

---
+++ src/mcp_server_unitycatalog/tools.py
@@ -0,0 +1,14 @@
+"""Unity Catalog Model Context Protocol (MCP) Server Tools.
+
+This module provides utility functions for interacting with the Unity Catalog AI MCP server.
+
+Features:
+- Lists Unity Catalog Functions.
+- Retrieves information about a specific Unity Catalog Function.
+- Creates Unity Catalog (Python) Functions.
+- Executes Unity Catalog (Python) Functions.
+- Deletes Unity Catalog Functions.
+
+License:
+MIT License (c) 2025 Shingo Okawa
+"""
